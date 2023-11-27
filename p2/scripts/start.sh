09d78fb325c142b150c0eec66ce8dbddab17e842
grvelva <grvelva@localhost.localdomain>
2023-11-27 21:27:10 +0100

    start service and app moved to the script

--- p2/scripts/start.sh
+++ p2/scripts/start.sh
@@ -1,4 +1,12 @@
 whoami
 curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644
 mkdir -p .kube
-sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
+cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
+whoami
+export NAMESPACE=grvelva
+echo $PATH
+export PATH="$PATH:/usr/local/bin"
+echo $PATH
+kubectl create namespace ${NAMESPACE}
+kubectl --namespace ${NAMESPACE} create -f /tmp/scripts/deployment.yaml
+kubectl --namespace ${NAMESPACE} create -f /tmp/scripts/service.yaml
