27254f44d98ef2de678fbc74a314aae7a78c7361
ddiakova <ddiakova@student42.fr>
2023-11-28 21:38:09 +0100

    k3s on master installed

--- p1/scripts/agent.sh
+++ p1/scripts/agent.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-export INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 --token-file $(sudo cat /vagrant/token) --node-ip=192.168.56.111"
+export INSTALL_K3S_EXEC="agent --write-kubeconfig-mode=644 --server https://192.168.56.110:6443 --token-file $(sudo cat /vagrant/token) --node-ip=192.168.56.111"
 curl -sfL https://get.k3s.io | sh -
-kubectl get nodes
--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-export INSTALL_K3S_EXEC="server --write-kubeconfig-mode=644 --bind-address=192.168.56.110 --advertise-address=192.168.56.110 --tls-san --node-ip 192.168.56.110"
+export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san serverS --node-ip 192.168.56.110 --bind-address=192.168.56.110 --advertise-address=192.168.56.110"
 curl -sfL https://get.k3s.io | sh -
-sudo cat /var/lib/rancher/k3s/server/node-token >> /vagrant/token
-systemctl status k3s
+sudo systemctl status k3s
 cat /etc/rancher/k3s/k3s.yaml
+sudo cat /var/lib/rancher/k3s/server/node-token >> /vagrant/token
 mkdir ~/.kube 
-cp /etc/rancher/k3s/k3s.yaml ~/.kube/config journalctl -xeu k3s
-kubectl get nodes
+sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+# journalctl -xeu k3s
+kubectl get nodes -o wide
