6c0f9be863eddae3397901f5e2200847d83788bc
ddiakova <ddiakova@student42.fr>
2023-12-09 21:35:30 +0100

    first part ready

--- p1/scripts/agent.sh
+++ p1/scripts/agent.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-export INSTALL_K3S_EXEC="agent --write-kubeconfig-mode=644 --server https://192.168.56.110:6443 --token-file $(sudo cat /vagrant/token) --node-ip=192.168.56.111"
+export INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 -t $(sudo cat /vagrant/token) --node-ip=192.168.56.111"
 curl -sfL https://get.k3s.io | sh -
+systemctl status k3s-agent.service
+
+sudo ip link add eth1 type dummy && sudo ip addr add 192.168.56.111/24 dev eth1 && sudo ip link set eth1 up
+
+sudo rm /vagrant/token
--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san serverS --node-ip 192.168.56.110 --bind-address=192.168.56.110 --advertise-address=192.168.56.110"
+export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san server --node-ip 192.168.56.110 --bind-address=192.168.56.110 --advertise-address=192.168.56.110"
 curl -sfL https://get.k3s.io | sh -
 sudo systemctl status k3s
 cat /etc/rancher/k3s/k3s.yaml
 sudo cat /var/lib/rancher/k3s/server/node-token >> /vagrant/token
 mkdir ~/.kube 
 sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
-# journalctl -xeu k3s
-kubectl get nodes -o wide
+
+sudo ip link add eth1 type dummy && sudo ip addr add 192.168.56.110/24 dev eth1 && sudo ip link set eth1 up
