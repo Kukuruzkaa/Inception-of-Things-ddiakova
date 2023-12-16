aaf340846348186fcd2e1f155a9dc44cf546f2f0
grvelva <grvelva@localhost.localdomain>
2023-12-16 17:15:48 +0100

    connected to the app from host

--- p2/scripts/start.sh
+++ p2/scripts/start.sh
@@ -3,10 +3,7 @@ curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644
 mkdir -p .kube
 cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
 whoami
-export NAMESPACE=grvelva
-echo $PATH
 export PATH="$PATH:/usr/local/bin"
-echo $PATH
 kubectl apply -f /tmp/scripts/deployment.yaml
 kubectl apply -f /tmp/scripts/service.yaml
 kubectl apply -f /tmp/scripts/ingress.yaml
