66766792341fd7d29d04981610bdee0b7cae9dfa
grvelva <grvelva@localhost.localdomain>
2023-12-17 18:40:51 +0100

    3 apps created

--- p2/scripts/start.sh
+++ p2/scripts/start.sh
@@ -4,6 +4,8 @@ mkdir -p .kube
 cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
 whoami
 export PATH="$PATH:/usr/local/bin"
-kubectl apply -f /tmp/scripts/deployment.yaml
-kubectl apply -f /tmp/scripts/service.yaml
-kubectl apply -f /tmp/scripts/ingress.yaml
+kubectl apply -f /tmp/scripts/service.yaml 
+kubectl apply -f /tmp/scripts/deployment-app-one.yaml 
+kubectl apply -f /tmp/scripts/deployment-app-two.yaml 
+kubectl apply -f /tmp/scripts/deployment-app-three.yaml 
+kubectl apply -f /tmp/scripts/ingress.yaml 
