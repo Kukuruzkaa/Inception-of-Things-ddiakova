189b9cfb0f5ad949542f1752bd487f60792e2aa5
Your Name <you@example.com>
2024-02-09 18:13:10 +0100

    fixes

--- p3/k3d.sh
+++ p3/k3d.sh
@@ -1,4 +1,5 @@
 sudo apt update -y
-sudo apt install -y curl git
+sudo snap install kubectl --classic
+sudo apt install -y curl git docker.io
 sudo curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
 sudo k3d cluster create part3cluster -p "8081:80@loadbalancer" --agents 2
--- p3/kube.sh
+++ p3/kube.sh
@@ -2,8 +2,10 @@
 export KUBECONFIG=$(sudo k3d kubeconfig write part3cluster)
 
 sudo kubectl create namespace argocd && sudo kubectl create namespace dev
+sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 sudo kubectl apply -f argocd-manifest.yml -n argocd
-sudo kubectl wait --for=condition=ready --timeout=1000s pod -all -n argocd
-sudo argocd admin initial-password -n argocd
+sudo kubectl wait --for=condition=ready --timeout=1000s pod --all -n argocd
+sudo chmod -R 777 /root
+argocd admin initial-password -n argocd
 sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 sudo kubectl port-forward svc/argocd-server -nargocd 8080:443
